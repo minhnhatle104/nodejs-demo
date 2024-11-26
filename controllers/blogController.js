76b9945ac5274f9f99c2c86fef47e5c41df27fe8
minhnhatle104 <lnminh104@gmail.com>
2024-11-26 10:31:57 +0700

    add imagePic to detail page

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,15 +1,15 @@
 const controller = {};
 const models = require("../models");
-const {Op} = require('sequelize');
+const { Op } = require('sequelize');
 
-controller.init = async(req, res, next) => {
+controller.init = async (req, res, next) => {
     res.locals.categories = await models.Category.findAll({
         include: [{ model: models.Blog }],
     });
     res.locals.tags = await models.Tag.findAll();
     next();
 }
-controller.showList = async(req, res) => {
+controller.showList = async (req, res) => {
     let limit = 2;
     let { category = 0, tag = 0, keyword = "", page = 1 } = req.query;
     category = isNaN(category) ? 0 : parseInt(category);
@@ -18,7 +18,7 @@ controller.showList = async(req, res) => {
     let offset = (page - 1) * limit;
 
     let options = {
-        include: [ { model: models.Comment } ],
+        include: [{ model: models.Comment }],
         where: {},
     };
 
@@ -27,10 +27,10 @@ controller.showList = async(req, res) => {
     }
 
     if (tag) {
-        options.include.push({model: models.Tag, where: {id: tag}});
+        options.include.push({ model: models.Tag, where: { id: tag } });
     }
 
-    if(keyword.trim() != "") {
+    if (keyword.trim() != "") {
         options.where[Op.or] = {
             title: { [Op.iLike]: `%${keyword.trim()}%` },
             summary: { [Op.iLike]: `%${keyword.trim()}%` },
@@ -42,26 +42,26 @@ controller.showList = async(req, res) => {
         distinct: true, col: "id",
     });
     res.locals.pagination = {
-        page, 
+        page,
         limit,
         totalRows,
         queryParams: req.query,
     }
 
-    let blogs = await models.Blog.findAll({...options, limit, offset});
+    let blogs = await models.Blog.findAll({ ...options, limit, offset });
     res.locals.blogs = blogs;
     res.render("index");
 };
 
-controller.showDetails = async(req, res) => {
+controller.showDetails = async (req, res) => {
     let id = isNaN(req.params.id) ? 0 : parseInt(req.params.id);
     res.locals.blog = await models.Blog.findOne({
-        where: {id},
+        where: { id },
         include: [
-            {model: models.Comment},
-            {model: models.User},
-            {model: models.Category},
-            {model: models.Tag},
+            { model: models.Comment },
+            { model: models.User },
+            { model: models.Category },
+            { model: models.Tag },
         ],
     });
     res.render("details");
